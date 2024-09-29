478e49d42cd660909d7d0a6935e29a1572d3d816
Abhishek <abhishekwani619@gmail.com>
2024-09-29 18:25:18 +0530

    notice page ui changes

--- src/components/NoticeComponent.vue
+++ src/components/NoticeComponent.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <p>Notices</p>
+        <v-card color="primary" title="NOTICES" class="text-center header-card mb-5"></v-card>
         <div v-for="i in notices" :key="i">
-            <v-card :variant="variant" class="mx-auto" color="surface-variant" max-width="344" :subtitle="i.description"
-                :title="i.title">
-                <template v-slot:actions>
+            <v-card :variant="elevated" class="mx-auto" color="#d5def5" max-width="344">
+                <div class="notice">
+                    <p class="title">{{ i.title }}</p>
+                    <p class="desc">{{ i.description }}</p>
+                    <p class="date">दिनांक - {{ i.date }}</p>
+                </div>
+                <div class="text-center pb-5">
                     <router-link :to="{ name: 'view-document', query: { id: i.url } }"><v-btn text="Open notice"></v-btn></router-link>
-                </template>
-                <p>{{ i.date }}</p>
+                </div>
+              
+
             </v-card>
         </div>
     </div>
@@ -31,3 +36,22 @@ export default {
     }
 }
 </script>
+
+<style>
+.notice .title{
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: 600;
+    padding-top: 10px;
+}
+.notice .desc {
+    padding: 2.5%;
+    font-size: 1.15rem;
+    font-weight: 400;
+}
+.notice .date{
+    padding: 2.5%;
+    font-size: 1.15rem;
+    font-weight: 400;
+}
+</style>
